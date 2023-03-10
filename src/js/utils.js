f86f6b5e8f29f9bd970823d0255d6096a3d69f71
Daniel John Kerschl <DanielJohnKerschl@googlemail.com>
2023-03-10 12:16:40 +0800

    [ADD] post sorting

--- src/js/utils.js
+++ src/js/utils.js
@@ -12,3 +12,42 @@ export const slugify = (text) => {
 export const formatDate = (date) => {
   return new Date(date).toLocaleDateString("de-DE", { timeZone: "GMT" });
 };
+
+export const formatBlogPosts = (
+  posts,
+  {
+    filterOutDrafts = true,
+    filterOutFuturePosts = true,
+    sortByDate = true,
+    limit = undefined,
+  } = {}
+) => {
+  const filteredPosts = posts.reduce((acc, post) => {
+    const { date, draft } = post.frontmatter;
+    // filterOutDrafts if true
+    if (filterOutDrafts && draft) return acc;
+
+    // filterOutFuturePosts if true
+    if (filterOutFuturePosts && new Date(date) > new Date()) return acc;
+
+    // add post to acc
+    acc.push(post);
+
+    return acc;
+  }, []);
+
+  // sortByDate or randomize
+  if (sortByDate) {
+    filteredPosts.sort(
+      (a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date)
+    );
+  } else {
+    filteredPosts.sort(() => Math.random() - 0.5);
+  }
+
+  // limit if number is passed
+  if (typeof limit === "number") {
+    return filteredPosts.slice(0, limit);
+  }
+  return filteredPosts;
+};
